b24620c0dd8a7c0849aa4110a6da1e4d0c2ffbf1
Storozukanton01 <43012572+Storozukanton01@users.noreply.github.com>
2019-04-25 10:59:33 +0300

    Update бінарний_пошук_вправо_вліво.cpp

--- бінарний_пошук_вправо_вліво.cpp
+++ бінарний_пошук_вправо_вліво.cpp
@@ -6,56 +6,69 @@ using namespace std;
 int main()
 
 {
- int n, m, mid;
+ int n, m, mid, res=-1;
  cin >> n >> m;
  vector<int> a(n);
  vector<int> b(m);
- int res=0, res1=0;
- for (int i = 0; i < n; ++i)
+ for (int i = 0; i < n; i++)
   {
  cin >> a[i];
   }
-  for (int i = 0; i < m; ++i)
+  for (int i = 0; i < m; i++)
   {
  cin >> b[i];
   }
+  int resl;
   for (int i = 0; i < m; i++)
     {
+    	 res=-1;
   int l = 0, r = n - 1;
-  while (l < r)
+  while(l <= r)
         {
-         mid = (l + r) / 2;
+            mid = (l + r) / 2;
             if (a[mid] == b[i])
-            {
-              res=mid;
-              r=mid-1;
-            }
-            else if(a[mid]>b[i])
-            {
-             r = mid - 1;
-            }
-            else 
-            {
-             l=mid + 1;
-            }
-        }
-        while (l < r)
+     		 {
+ 			   res=mid;
+ 			   r=mid-1;
+ 		     }
+           	   else if(a[mid]>b[i])
+             {
+               r = mid-1;
+             }
+             else
+             {
+             	l=mid+1;
+			 }
+         }
+	l=0;
+	r=n-1;
+
+	while(l <= r)
         {
-         mid = (l + r) / 2;
+            mid = (l + r) / 2;
             if (a[mid] == b[i])
-            {
-              res1=mid;
-              l=mid+1;
-            }
-            else if(a[mid]>b[i])
-            {
-             r = mid - 1;
-            }
-            else 
-            {
-             l=mid + 1;
-            }
-        }
-        cout<<res<<" "<<res1<<endl;
+     		 {
+ 			   resl=mid;
+ 			   l=mid+1;
+ 		     }
+           	   else if(a[mid]>b[i])
+             {
+               r=mid-1;
+             }
+             else
+             {
+            	l=mid+1;
+			 }
+         }
+         if(res!=-1)
+         {
+         	res++;
+         	resl++;
+         	cout<<res<<" "<<resl<<endl;
+		 }
+		 else
+		 {
+		 	cout<<"0"<<endl;
+		 }
     }
 }
